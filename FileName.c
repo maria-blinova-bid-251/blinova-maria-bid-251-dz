61162c373749c94c167d598871c7ab50afcb8f60
maria-blinova-bid-251 <tyanmash@yandex.ru>
2025-09-14 17:02:40 +0300

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,16 @@
+﻿#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts(" _");
+	puts("( \\");
+	puts(" ) )");
+	puts("( (  .-''''-.  A.-.A");
+	puts(" \\ \\/        \\/ , , \\");
+	puts("  \\   \\      =;  t  /=");
+	puts("   \\   |''''.  ',--'");
+	puts("   /  //    | ||");
+	puts("  /_,))     |_,))");
+	getchar();
+}
